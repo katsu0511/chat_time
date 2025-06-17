02111adb2e2a0044d5b6c1cd5cab036aaf00b9ef
Katsuya <haradakatsuya190511@gmail.com>
2025-06-16 23:48:30 -0700

    added tailwind css setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
         <div className="chat_screen"></div>
       </main>
       <footer>
-        <a>Chat</a>
-        <a>Friends</a>
-        <a>Settings</a>
+        <a href="" className="bg-blue-600 text-white px-4 py-2">Chat</a>
+        <a href="" className="bg-blue-600 text-white px-4 py-2">Friends</a>
+        <a href="" className="bg-blue-600 text-white px-4 py-2">Settings</a>
       </footer>
     </div>
   );
