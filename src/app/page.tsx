55307a51bd643102a8361a2061a78334317f6249
Katsuya <haradakatsuya190511@gmail.com>
2025-06-18 23:25:52 -0700

    added input html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ export default function Home() {
       <main className="h-screen">
         <div className="flex justify-center w-[1200px] h-full mx-auto my-0">
           <div className="chat_rooms w-3/10 h-full bg-white border-blue-500 border-x-4"></div>
-          <div className="chat_screen w-7/10 h-full bg-blue-100"></div>
+          <div className="w-7/10 h-full">
+            <div className="chat_screen bg-blue-100 w-full h-9/10"></div>
+            <div className="w-full h-1/10">
+              <input type="text" className="bg-white border-blue-500 border-2 px-2 py-1" />
+              <input type="button" value="Send" className="bg-white text-blue-500 border-blue-500 border-2 h-6 w-10" />
+            </div>
+          </div>
         </div>
       </main>
       <footer className="bg-blue-500 h-10">
