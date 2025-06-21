4dc9c3a683f31a447e08a1e4bf6a7cfd658d51b4
Katsuya <haradakatsuya190511@gmail.com>
2025-06-20 19:49:39 -0700

    added header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import type { Metadata } from "next";
 import { Inconsolata } from 'next/font/google';
 import "./globals.css";
@@ -17,12 +18,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={fnt.className}>
+        <header className="bg-blue-500 h-10">
+          <div className="w-[1200px] mx-auto my-0">
+            <Link href="/" className="text-white text-3xl font-bold leading-10 duration-300 hover:opacity-60">Chat Time</Link>
+          </div>
+        </header>
         {children}
         <footer className="bg-blue-500 h-10">
           <div className="flex justify-center w-[1200px] mx-auto my-0">
-            <a href="" className="block bg-white text-blue-500 border-blue-500 border-l-4 border-r-2 border-y-4 text-lg font-bold text-center h-10 w-50 duration-300 hover:bg-blue-500 hover:text-white">Chat</a>
-            <a href="" className="block bg-white text-blue-500 border-blue-500 border-x-2 border-y-4 text-lg font-bold text-center h-10 w-50 duration-300 hover:bg-blue-500 hover:text-white">Friends</a>
-            <a href="" className="block bg-white text-blue-500 border-blue-500 border-r-4 border-l-2 border-y-4 text-lg font-bold text-center h-10 w-50 duration-300 hover:bg-blue-500 hover:text-white">Settings</a>
+            <Link href="" className="block bg-white text-blue-500 border-blue-500 border-l-4 border-r-2 border-y-4 text-lg font-bold text-center h-10 w-50 duration-300 hover:bg-blue-500 hover:text-white">Chat</Link>
+            <Link href="" className="block bg-white text-blue-500 border-blue-500 border-x-2 border-y-4 text-lg font-bold text-center h-10 w-50 duration-300 hover:bg-blue-500 hover:text-white">Friends</Link>
+            <Link href="" className="block bg-white text-blue-500 border-blue-500 border-r-4 border-l-2 border-y-4 text-lg font-bold text-center h-10 w-50 duration-300 hover:bg-blue-500 hover:text-white">Settings</Link>
           </div>
         </footer>
       </body>
