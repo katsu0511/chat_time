0161dee3072b5fcb57c7534a5185c5595eae2466
Katsuya <haradakatsuya190511@gmail.com>
2025-06-26 18:38:11 -0700

    created a getter file

---
+++ src/lib/getter.ts
@@ -0,0 +1,19 @@
+import prisma from './prisma';
+import type { User } from 'next-auth';
+
+export async function getUser(userid: string, password: string): Promise<User | null> {
+  const userInfo = await prisma.user.findUnique({
+    where: {
+      email: userid,
+      password: password
+    }
+  });
+
+  if (!userInfo) return null;
+
+  return {
+    id: userInfo.id as unknown as string,
+    name: userInfo.name,
+    email: userInfo.email
+  }
+}
