d1d3d0d403ce7e2e55adad8ac0dffdb2379cacfa
Katsuya <haradakatsuya190511@gmail.com>
2025-07-02 22:27:10 -0700

    modularize footer link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
+import './globals.css';
+import { Inconsolata } from 'next/font/google';
+import type { Metadata } from 'next';
+import { getAuthSession } from '@/lib/getSession';
 import Link from 'next/link';
 import LogoutButton from '@/components/LogoutButton';
-import { getAuthSession } from '@/lib/getSession';
-import type { Metadata } from 'next';
-import { Inconsolata } from 'next/font/google';
-import './globals.css';
+import FooterLink from '@/components/FooterLink';
 
 const fnt = Inconsolata({ subsets: ['latin'] });
 
@@ -37,11 +38,14 @@ export default async function RootLayout({
           </div>
         </main>
         <footer className="bg-blue-500 w-full h-10">
-          <div className="flex justify-center w-[1200px] mx-auto my-0">
-            <Link href="" className="block bg-white text-blue-500 border-blue-500 border-l-4 border-r-2 border-y-4 text-lg font-bold text-center h-10 w-50 duration-300 hover:bg-blue-500 hover:text-white">Chat</Link>
-            <Link href="" className="block bg-white text-blue-500 border-blue-500 border-x-2 border-y-4 text-lg font-bold text-center h-10 w-50 duration-300 hover:bg-blue-500 hover:text-white">Friends</Link>
-            <Link href="" className="block bg-white text-blue-500 border-blue-500 border-r-4 border-l-2 border-y-4 text-lg font-bold text-center h-10 w-50 duration-300 hover:bg-blue-500 hover:text-white">Settings</Link>
-          </div>
+          {session
+            ? <div className="flex justify-center w-[1200px] mx-auto my-0">
+                <FooterLink display='Chat' url='/'/>
+                <FooterLink display='Friends' url='/friends'/>
+                <FooterLink display='Settings' url='/settings'/>
+              </div>
+            : <div className='text-white text-xl w-[1200px] leading-10 text-center mx-auto my-0'>&copy;Chat time</div>
+          }
         </footer>
       </body>
     </html>
