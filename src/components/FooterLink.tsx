4acb593cf84f1652de419fbce0bdfb837cd105d9
Katsuya <haradakatsuya190511@gmail.com>
2025-07-03 16:19:11 -0700

    modified css

--- src/components/FooterLink.tsx
+++ src/components/FooterLink.tsx
@@ -2,6 +2,6 @@ import Link from 'next/link';
 
 export default function FooterLink({display, url}: {display: string, url: string}) {
   return (
-    <Link href={`.${url}`} className='block bg-white text-blue-500 border-blue-500 border-x-2 border-y-4 text-lg font-bold text-center h-10 w-50 duration-300 hover:bg-blue-500 hover:text-white'>{display}</Link>
+    <Link href={`.${url}`} aria-label={display} className='block bg-white text-blue-500 border-blue-500 border-x-2 border-y-4 text-lg font-bold text-center h-10 w-50 duration-300 hover:bg-blue-500 hover:text-white'>{display}</Link>
   );
 }
