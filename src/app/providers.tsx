b03da915f0b5985984f68d6e62180744b0d66282
Katsuya <haradakatsuya190511@gmail.com>
2025-07-03 17:27:32 -0700

    modified providers.tsx a bit

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { ReactNode } from 'react';
 import { SessionProvider } from 'next-auth/react';
 
-export default function Provider({ children }: { children: ReactNode }) {
+export default function Provider({ children }: { children: React.ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
