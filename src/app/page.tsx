eb3716da0952dbc7780f3c9eddd4129e1bcab460
Katsuya <haradakatsuya190511@gmail.com>
2025-07-05 14:51:42 -0700

    modularize send message elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { getAuthSession } from '@/lib/getSession';
 import { redirect } from 'next/navigation';
-import { Input, Button } from '@mui/material';
-import { blue } from '@mui/material/colors';
+import SendMessage from '@/components/SendMessage';
 
 export default async function Home() {
   const session = await getAuthSession();
@@ -15,41 +14,7 @@ export default async function Home() {
       <div className='w-3/10 h-full'></div>
       <div className='w-7/10 h-full'>
         <div className='bg-blue-100 w-full h-19/20'></div>
-        <div className='flex w-full h-1/20'>
-          <Input
-            disableUnderline
-            sx={{
-              display: 'block',
-              width: '90%',
-              height: '100%'
-            }}
-            inputProps={{
-              sx: {
-                display: 'block',
-                border: '2px solid',
-                borderColor: blue[400],
-                boxSizing: 'border-box',
-                width: '100%',
-                height: '100%',
-                p: 1,
-                appearance: 'none'
-              }
-            }}
-          />
-          <Button
-            variant='contained'
-            color='secondary'
-            disableElevation={true}
-            sx={{
-              display: 'block',
-              width: '10%',
-              height: '100%',
-              borderRadius: '0'
-            }}
-          >
-            Send
-          </Button>
-        </div>
+        <SendMessage />
       </div>
     </div>
   );
