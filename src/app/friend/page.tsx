311c2ad76a75e04031f48d4f8164c661fe666c77
Katsuya <haradakatsuya190511@gmail.com>
2025-07-12 02:11:36 -0700

    modularized SearchUsers

--- src/app/friend/page.tsx
+++ src/app/friend/page.tsx
@@ -1,57 +1,9 @@
-'use client';
+import { getAuthSession } from '@/lib/getSession';
+import { redirect } from 'next/navigation';
+import SearchUsers from '@/components/searchUsers';
 
-import { useState } from "react";
-import type { User } from 'next-auth';
-import { Input } from "@mui/material";
-import { blue } from '@mui/material/colors';
-
-export default function Friend() {
-  const [users, setUsers] = useState<React.ReactNode[]>();
-
-  const searchUsers = async (name: string) => {
-    name = name.trim();
-    if (!name) {
-      setUsers([]);
-      return;
-    }
-
-    const res = await fetch(`/api/getUsersByName?name=${name}`);
-    if (!res.ok)
-      setUsers([]);
-    else {
-      const json: User[] = await res.json();
-      const infos = json.map(value => (
-        <li key={value.id}>Name: {value.name}, User ID: {value.userid}</li>
-      ));
-      setUsers(infos);
-    }
-  };
-
-  return (
-    <div className='w-full h-full py-20'>
-      <Input
-        disableUnderline
-        sx={{
-          display: 'block',
-          width: '400px',
-          height: '50px',
-          margin: '0 auto'
-        }}
-        inputProps={{
-          sx: {
-            display: 'block',
-            border: '2px solid',
-            borderColor: blue[500],
-            boxSizing: 'border-box',
-            width: '100%',
-            height: '100%',
-            p: 1,
-            appearance: 'none'
-          }
-        }}
-        onChange={(e) => searchUsers(e.target.value)}
-      />
-      <ul className='block text-center py-10'>{users}</ul>
-    </div>
-  );
+export default async function Friend() {
+  const session = await getAuthSession();
+  if (!session) return redirect('/login');
+  return <SearchUsers />;
 }
