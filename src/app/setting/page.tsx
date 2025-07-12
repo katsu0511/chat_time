35537d73b71f59a078e0573e9adf07af00658be4
Katsuya <haradakatsuya190511@gmail.com>
2025-07-12 02:12:24 -0700

    refactoring a bit

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,7 +1,12 @@
+import { getAuthSession } from '@/lib/getSession';
+import { redirect } from 'next/navigation';
 import { FormControl } from '@mui/material';
 import RadioGroupWrapper from '@/components/RadioGroupWrapper';
 
-export default function Setting() {
+export default async function Setting() {
+  const session = await getAuthSession();
+  if (!session) return redirect('/login');
+
   return (
     <div className='flex justify-center w-full h-full'>
       <FormControl>
