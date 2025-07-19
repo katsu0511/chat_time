99bfda85457225f01d8f51cf87ebdde2c2e04cd4
Katsuya <haradakatsuya190511@gmail.com>
2025-07-19 13:35:25 -0700

    moduralized SendMessage.tsx

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -50,7 +50,7 @@ export default function SearchUsers({session}: {session: Session}) {
       </div>
       <div className='w-7/10 h-full'>
         <div className='bg-blue-100 w-full h-19/20'>{messages}</div>
-        <SendMessage />
+        <SendMessage senderId={session.user.id as unknown as number} receiverId={receiverId} />
       </div>
     </div>
   );
