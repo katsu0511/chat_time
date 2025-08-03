08ddc9e06cb851ca04f6d87338380c7de24ede69
Katsuya <haradakatsuya190511@gmail.com>
2025-08-03 13:02:44 -0700

    use ThemeWrapper and RadioButton

--- src/components/ThemeModeWrapper.tsx
+++ src/components/ThemeModeWrapper.tsx
@@ -2,39 +2,18 @@
 
 import { useContext } from 'react';
 import { ThemeContext } from '@/components/ThemeProviderWrapper';
-import { FormControlLabel, RadioGroup, Radio, FormLabel } from '@mui/material';
+import ThemeWrapper from './ThemeWrapper';
+import RadioButton from './RadioButton';
 
 export default function ThemeModeWrapper() {
   const context = useContext(ThemeContext);
   if (!context) return null;
-  const { mode, theme, toggleMode } = context;
+  const { mode, toggleMode } = context;
 
   return (
-    <div className='flex flex-wrap justify-center items-center w-full h-auto my-10'>
-      <FormLabel
-        component='legend'
-        sx={{
-          color: theme.palette.primary.main,
-          width: '100%',
-          textAlign: 'center'
-        }}
-      >
-        Theme
-      </FormLabel>
-      <RadioGroup
-        name='theme'
-        value={mode}
-        onChange={toggleMode}
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          margin: '0 20px'
-        }}
-        row
-      >
-        <FormControlLabel value='light' control={<Radio />} label='Light' />
-        <FormControlLabel value='dark' control={<Radio />} label='Dark' />
-      </RadioGroup>
-    </div>
+    <ThemeWrapper label='Theme' value={mode} toggle={toggleMode}>
+      <RadioButton value='light' label='Light' />
+      <RadioButton value='dark' label='Dark' />
+    </ThemeWrapper>
   );
 }
