b25c03867f7218d59fc1ea41a42dbef2c5f29d08
Katsuya <haradakatsuya190511@gmail.com>
2025-08-03 13:02:12 -0700

    use ThemeWrapper and RadioButton

--- src/components/ThemeColorWrapper.tsx
+++ src/components/ThemeColorWrapper.tsx
@@ -1,48 +1,21 @@
 'use client';
 
-import { ThemeColor } from '@/lib/themeColor';
 import { useContext } from 'react';
 import { ThemeContext } from '@/components/ThemeProviderWrapper';
-import { FormControlLabel, RadioGroup, Radio, FormLabel } from '@mui/material';
+import ThemeWrapper from './ThemeWrapper';
+import { ThemeColor } from '@/lib/themeColor';
+import RadioButton from './RadioButton';
 
 export default function ThemeColorWrapper() {
   const context = useContext(ThemeContext);
   if (!context) return null;
-  const { color, theme, toggleColor } = context;
+  const { color, toggleColor } = context;
 
   return (
-    <div className='flex flex-wrap justify-center items-center w-full h-auto my-10'>
-      <FormLabel
-        component='legend'
-        sx={{
-          color: theme.palette.primary.main,
-          width: '100%',
-          textAlign: 'center'
-        }}
-      >
-        Color
-      </FormLabel>
-      <RadioGroup
-        name='color'
-        value={color}
-        onChange={(e) => toggleColor(e.target.value as ThemeColor)}
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          margin: '0 20px'
-        }}
-        row
-      >
-        <FormControlLabel value={ThemeColor.blue} control={<Radio />} label='Blue' />
-        <FormControlLabel value={ThemeColor.red} control={<Radio />} label='Red' />
-        <FormControlLabel value={ThemeColor.yellow} control={<Radio />} label='Yellow' />
-        <FormControlLabel value={ThemeColor.purple} control={<Radio />} label='Purple' />
-        <FormControlLabel value={ThemeColor.green} control={<Radio />} label='Green' />
-        <FormControlLabel value={ThemeColor.orange} control={<Radio />} label='Orange' />
-        <FormControlLabel value={ThemeColor.brown} control={<Radio />} label='Brown' />
-        <FormControlLabel value={ThemeColor.pink} control={<Radio />} label='Pink' />
-        <FormControlLabel value={ThemeColor.grey} control={<Radio />} label='Grey' />
-      </RadioGroup>
-    </div>
+    <ThemeWrapper label='Color' value={color} toggle={toggleColor}>
+      {Object.values(ThemeColor).map((color) => (
+        <RadioButton key={color} value={color} label={color.charAt(0).toLocaleUpperCase() + color.substring(1)} />
+      ))}
+    </ThemeWrapper>
   );
 }
