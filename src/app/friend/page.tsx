6c8ccde637658a7a1ac65b5f06f53cb1aec22dc8
Katsuya <haradakatsuya190511@gmail.com>
2025-08-19 13:38:25 -0700

    modified a bit

--- src/app/friend/page.tsx
+++ src/app/friend/page.tsx
@@ -6,7 +6,6 @@ import { getFriendIds } from '@/lib/getter';
 export default async function Friend() {
   const session = await getAuthSession();
   if (!session) return redirect('/login');
-
   const friendIds = await getFriendIds(Number(session.user.id));
   return <SearchUsers session={session} friendIds={friendIds} />;
 }
